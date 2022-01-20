7474aacd2c76f025060bc228da9adc82fe855d2f
Victor Hugo <victor.hugolk10@gmail.com>
2022-01-20 20:22:16 -0300

    Adcionando o Typescript e incrementando e automatizando a forma de desenvolver

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response, NextFunction} from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) =>{
+    res.status(200).send({ foo: 'Acesso total!'})
+})
+
+
+app.listen(3000, () => {
+    console.log('Aplicação rodando na porta 3000!')
+})
